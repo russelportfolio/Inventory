345ec146db8481875c7ab70857c99e85e1d9e25f
russe <johnrusselsauli.portfolio@gmail.com>
2025-05-19 23:19:42 +0800

    initial commit

---
+++ common-main/src/main/java/com/russel/portfolio/common_main/CommonMainApplication.java
@@ -0,0 +1,13 @@
+package com.russel.portfolio.common_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonMainApplication.class, args);
+	}
+
+}
---
+++ common-main/src/test/java/com/russel/portfolio/common_main/CommonMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russel.portfolio.common_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
